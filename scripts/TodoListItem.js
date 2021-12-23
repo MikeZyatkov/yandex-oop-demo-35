6f3d5479a323c3b4e1c657b11169ac532af58206
Mike Zyatkov <mikhail.zyatkov@essensys.tech>
2021-12-23 17:29:15 +0000

    delete todo

--- scripts/TodoListItem.js
+++ scripts/TodoListItem.js
@@ -11,9 +11,14 @@ class TodoListItem {
             .cloneNode(true)
     }
 
+    _handleDeleteTodo = () => {
+        this._element.remove();
+    }
+
     getView() {
         this._element = this._getTemplate();
         this._element.querySelector('.todo-item__text').textContent = this._title;
+        this._element.querySelector('.todo-item__del').addEventListener('click', this._handleDeleteTodo);
 
         return this._element;
     }
